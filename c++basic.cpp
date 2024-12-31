c4586caa56994a66aaec46f8405f18491db63c42
Shihab Hossain <hossainshihab2001@gmail.com>
2024-12-31 11:58:10 +0600

    Add files via upload

---
+++ c++basic.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h> //all library in c++
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+//     int x=19;
+//     char c ='a';
+//     double d= 22.33;
+//     std::cout<< x <<" "<< c << std:: endl << d;
+//    std::cout<<" 1234566";
+//    std:: cout<<"my fvrt numb is"<< x;
+// int t;
+// char a= 'a';
+//     cin>> t;
+//     cout<< t <<(int)a << endl;
+
+
+//     int s;
+//     while (cin>>s){
+//         cout<<s;
+//     }
+
+    // double d=44.23454;
+    // cout<< fixed<<setprecision(5)<<d;
+
+
+    // int p=50;
+    // p%2==0? cout<< "even" : cout<<"odd";
+
+
+    // int u;
+    // cin>>u;
+    // switch(u%2){
+    //     case 0:
+    //     cout<<"even";
+    //     break;
+    //     case 1:
+    //     cout<<"odd";
+    //     break;
+    // }
+
+
+
+    // int y,z,l;
+    // cin>> y>> z;
+    // cout<< min(y,z);
+    // cout<< max(y,z);
+
+    // swap(y,z);
+    // cout<<y<<" "<<z;
+
+    char e[100];
+    cin>> e;
+    cout<<e;
+
+//     int *q =new int; //dynamic heap memory declare
+//     *q=100;
+//     int k[6];           //static array
+//    int *k =new int[6]; //dynamic array... benifit is size baranu jai and return krleo memory te teke jai
+  //  delete[] k;         // in dynamic we can delete
+   
+   return 0;
+}
